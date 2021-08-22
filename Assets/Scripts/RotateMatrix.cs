9489f34206e3011ae952f80b78ce6a48329adb3c
emeldi <78385236+emeldi@users.noreply.github.com>
2021-08-22 13:34:25 -0700

    completed bonus project

--- Assets/Scripts/RotateMatrix.cs
+++ Assets/Scripts/RotateMatrix.cs
@@ -8,10 +8,10 @@ public class RotateMatrix : MonoBehaviour
 {
     // Initial data assigned in inspector
     [SerializeField]
-    private int[] _initialArray9 = new int[9];
+    private int[] _initialArray16 = new int[16];
 
     // Declare variables for base matrices to evaluate
-    private int[,] _baseMatrix3x3 = new int[3, 3];
+    private int[,] _baseMatrix4x4 = new int[4, 4];
 
     // Serialized lists for the matrix visualizer text
     [SerializeField]
@@ -34,9 +34,9 @@ public class RotateMatrix : MonoBehaviour
 
         while (r > 0)
         {
-            int[,] rotatedMatrix = new int[3, 3];
-            rotatedMatrix = RotateClockwise(_baseMatrix3x3);
-            _baseMatrix3x3 = rotatedMatrix;
+            int[,] rotatedMatrix = new int[4, 4];
+            rotatedMatrix = RotateClockwise(_baseMatrix4x4);
+            _baseMatrix4x4 = rotatedMatrix;
             UpdateVisualizer();
             r--;
         }
@@ -47,11 +47,11 @@ public class RotateMatrix : MonoBehaviour
         // Iterate rotations
         int r = _rotations;
 
-        while (_rotations > 0)
+        while (r > 0)
         {
-            int[,] rotatedMatrix = new int[3, 3];
-            rotatedMatrix = RotateCounterClockwise(_baseMatrix3x3);
-            _baseMatrix3x3 = rotatedMatrix;
+            int[,] rotatedMatrix = new int[4, 4];
+            rotatedMatrix = RotateCounterClockwise(_baseMatrix4x4);
+            _baseMatrix4x4 = rotatedMatrix;
             UpdateVisualizer();
             r--;
         }
@@ -68,8 +68,8 @@ public class RotateMatrix : MonoBehaviour
         int width;
         int height;
 
-        width = _baseMatrix3x3.GetUpperBound(0) + 1;
-        height = _baseMatrix3x3.GetUpperBound(1) + 1;
+        width = _baseMatrix4x4.GetUpperBound(0) + 1;
+        height = _baseMatrix4x4.GetUpperBound(1) + 1;
 
         // Iterate through 1D array
         int i = 0;
@@ -78,8 +78,8 @@ public class RotateMatrix : MonoBehaviour
         {
             for (int row = 0; row < height; row++)
             {
-                _baseMatrix3x3[column, row] = _initialArray9[i];
-                _cellText[i].text = _initialArray9[i].ToString();
+                _baseMatrix4x4[column, row] = _initialArray16[i];
+                _cellText[i].text = _initialArray16[i].ToString();
                 i++;
             }
         }
@@ -90,8 +90,8 @@ public class RotateMatrix : MonoBehaviour
         int width;
         int height;
 
-        width = _baseMatrix3x3.GetUpperBound(0) + 1;
-        height = _baseMatrix3x3.GetUpperBound(1) + 1;
+        width = _baseMatrix4x4.GetUpperBound(0) + 1;
+        height = _baseMatrix4x4.GetUpperBound(1) + 1;
 
         // Iterate through 1D array
         int i = 0;
@@ -100,66 +100,199 @@ public class RotateMatrix : MonoBehaviour
         {
             for (int row = 0; row < height; row++)
             {
-                _cellText[i].text = _baseMatrix3x3[column, row].ToString();
+                _cellText[i].text = _baseMatrix4x4[column, row].ToString();
                 i++;
             }
         }
     }
 
     
+    //private int[,] RotateClockwise(int[,] matrix)
+    //{
+    //    int width;
+    //    int height;
+    //    System.Numerics.Vector2 center;
+
+    //    width = matrix.GetUpperBound(0) + 1;
+    //    height = matrix.GetUpperBound(1) + 1;
+    //    float x = (width - 1) / 2.0f;
+    //    float y = (height - 1) / 2.0f;
+    //    Debug.Log(x + ", " + y);
+    //    center = new System.Numerics.Vector2(x, y);
+
+    //    int[,] result = new int[height, width];
+    //    for (int row = 0; row < height; row++)
+    //    {
+    //        for (int col = 0; col < width; col++)
+    //        {
+    //            System.Numerics.Vector2 vector = new System.Numerics.Vector2(row, col);
+    //            System.Numerics.Matrix3x2 rotationMatrix = 
+    //                System.Numerics.Matrix3x2.CreateRotation(0.7853982f, center); //0.7853982 is 45deg in radians
+    //            System.Numerics.Vector2 rotatedVector = System.Numerics.Vector2.Transform(vector, rotationMatrix);
+    //            Debug.Log("Original vector = " + vector.Y + ", " + vector.X + 
+    //                ", Rotated vector = " + rotatedVector.Y + ", " + rotatedVector.X);
+    //            // Expected [0,0] to become [1, 0] but here we're getting [-1, 2]
+    //            result[Mathf.RoundToInt(rotatedVector.Y), Mathf.RoundToInt(rotatedVector.X)] = matrix[col, row];
+    //        }
+    //    }
+    //    return result;
+    //}
+
     private int[,] RotateClockwise(int[,] matrix)
     {
         int width;
-        int height;
-        System.Numerics.Vector2 center;
+        int middle;
 
         width = matrix.GetUpperBound(0) + 1;
-        height = matrix.GetUpperBound(1) + 1;
-        float x = (width - 1) / 2.0f;
-        float y = (height - 1) / 2.0f;
-        Debug.Log(x + ", " + y);
-        center = new System.Numerics.Vector2(x, y);
 
-        int[,] result = new int[height, width];
-        for (int row = 0; row < height; row++)
+        middle = width / 2;
+        int[,] result = new int[width, width];
+
+        for (int row = 0; row < width; ++row)
         {
-            for (int col = 0; col < width; col++)
+            int corner = width - row - 1;
+            bool firsthalf = row < middle;
+            for (int column = 0; column < width; ++column)
             {
-                System.Numerics.Vector2 vector = new System.Numerics.Vector2(row, col);
-                System.Numerics.Matrix3x2 rotationMatrix = 
-                    System.Numerics.Matrix3x2.CreateRotation(0.7853982f, center); //0.7853982 is 45deg in radians
-                System.Numerics.Vector2 rotatedVector = System.Numerics.Vector2.Transform(vector, rotationMatrix);
-                result[Mathf.RoundToInt(rotatedVector.Y), Mathf.RoundToInt(rotatedVector.X)] = matrix[col, row];
+                int value = matrix[row, column];
+                Debug.Log("Moving " + value);
+                Debug.Log("Checking " + column + ", " + row);
+                if (firsthalf)
+                {
+                    if (column < row)
+                    {
+                        result[row - 1, column] = value;
+                    }
+                    else if (column == row)
+                    {
+                        result[row, column + 1] = value;
+                    }
+                    else if (column < corner)
+                    {
+                        result[row, column + 1] = value;
+                    }
+                    else
+                    {
+                        result[row + 1, column] = value;
+                    }
+                }
+                else
+                {
+                    if (column <= corner)
+                    {
+                        result[row - 1, column] = value;
+                    }
+                    //else if (column < row)
+                    //{
+                    //    result[row - 1, column] = value;
+                    //}
+                    else if (column > row)
+                    {
+                        result[row + 1, column] = value;
+                    }
+                    else
+                    {
+                        result[row, column -1] = value;
+                    }
+                }
             }
         }
+
         return result;
     }
+
+
+    //private int[,] RotateCounterClockwise(int[,] matrix)
+    //{
+    //    int width;
+    //    int height;
+    //    System.Numerics.Vector2 center;
+
+    //    width = matrix.GetUpperBound(0) + 1;
+    //    height = matrix.GetUpperBound(1) + 1;
+    //    float x = (width - 1) / 2.0f;
+    //    float y = (height - 1) / 2.0f;
+    //    center = new System.Numerics.Vector2(x, y);
+
+    //    int[,] result = new int[height, width];
+    //    for (int row = 0; row < height; row++)
+    //    {
+    //        for (int col = 0; col < width; col++)
+    //        {
+    //            System.Numerics.Vector2 vector = new System.Numerics.Vector2(row, col);
+    //            System.Numerics.Matrix3x2 rotationMatrix = 
+    //                System.Numerics.Matrix3x2.CreateRotation(-0.7853982f, center); // -0.7853982 is -45deg in radians
+    //            System.Numerics.Vector2 rotatedVector = System.Numerics.Vector2.Transform(vector, rotationMatrix);
+    //            Debug.Log("Original vector = " + vector.Y + ", " + vector.X +
+    //                ", Rotated vector = " + rotatedVector.Y + ", " + rotatedVector.X);
+    //            // Expected [0,0] -> [0, 1] but here we're getting [2, -1]
+    //            result[Mathf.RoundToInt(rotatedVector.Y), Mathf.RoundToInt(rotatedVector.X)] = matrix[col, row];
+    //        }
+    //    }
+    //    return result;
+    //}
 
     private int[,] RotateCounterClockwise(int[,] matrix)
     {
         int width;
-        int height;
-        System.Numerics.Vector2 center;
+        int middle;
 
         width = matrix.GetUpperBound(0) + 1;
-        height = matrix.GetUpperBound(1) + 1;
-        float x = (width - 1) / 2.0f;
-        float y = (height - 1) / 2.0f;
-        center = new System.Numerics.Vector2(x, y);
 
-        int[,] result = new int[height, width];
-        for (int row = 0; row < height; row++)
+        middle = width / 2;
+        int[,] result = new int[width, width];
+
+        for (int row = 0; row < width; ++row)
         {
-            for (int col = 0; col < width; col++)
+            int corner = width - row - 1;
+            bool firsthalf = row < middle;
+            for (int column = 0; column < width; ++column)
             {
-                System.Numerics.Vector2 vector = new System.Numerics.Vector2(row, col);
-                System.Numerics.Matrix3x2 rotationMatrix = 
-                    System.Numerics.Matrix3x2.CreateRotation(-0.7853982f, center); //-0.7853982 is -45deg in radians
-                System.Numerics.Vector2 rotatedVector = System.Numerics.Vector2.Transform(vector, rotationMatrix);
-                result[Mathf.RoundToInt(rotatedVector.Y), Mathf.RoundToInt(rotatedVector.X)] = matrix[col, row];
+                int value = matrix[row, column];
+
+                if (firsthalf)
+                {
+
+                    if (column < row)
+                    {
+                        result[row + 1, column] = value;
+                    }
+                    else if (column == row)
+                    {
+                        result[row + 1, column] = value;
+                    }
+                    else if (column <= corner)
+                    {
+                        result[row, column - 1] = value;
+                    }
+                    else
+                    {
+                        result[row - 1, column] = value;
+                    }
+
+                }
+                else
+                {
+
+                    if (column < corner)
+                    {
+                        result[row + 1, column] = value;
+                    }
+                    else if (column < row)
+                    {
+                        result[row, column + 1] = value;
+                    }
+                    else
+                    {
+                        result[row - 1, column] = value;
+                    }
+
+                }
             }
         }
+
         return result;
     }
+
 }
 
